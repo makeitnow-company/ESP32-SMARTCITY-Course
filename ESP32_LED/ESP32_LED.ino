3bf546519e72e4701e955f9fac4409fb87f5bd40
kkongnamul11 <33644487+kkongnamul11@users.noreply.github.com>
2022-03-23 18:39:52 +0900

    Add files via upload
    1. 신호등 코드_22.03.23

---
+++ ESP32_LED/ESP32_LED.ino
@@ -0,0 +1,24 @@
+#define LED_RED 4
+#define LED_YELLOW 5
+#define LED_GREEN 12 
+
+void setup() {
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_RED,HIGH);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, LOW);
+  delay(1000);
+  digitalWrite(LED_RED,HIGH);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, LOW);
+  delay(1000);
+  digitalWrite(LED_RED,HIGH);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, LOW);
+  delay(1000);
+}
